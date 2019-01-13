de2acd8329906219289cb93283b1d2ee6fb0b2a2
AlexMukha <alexmukha@email.com>
2019-01-12 21:04:29 -0800

    Final steps

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,8 +27,14 @@ $(document).ready(function () {
         var gems = shuffle(jewels); // Shuffle gem order
 
         var gem1 = gems[0];
+        var gem2 = gems[1];
+        var gem3 = gems[2];
+        var gem4 = gems[3];
         $("#gem1").html("<img src=\"./assets/images/" + gem1 + ".png\" alt=\"First Crystal\" title=\"" + gem1 + "\">");
-
+        $("#gem2").html("<img src=\"./assets/images/" + gem2 + ".png\" alt=\"First Crystal\" title=\"" + gem2 + "\">");
+        $("#gem3").html("<img src=\"./assets/images/" + gem3 + ".png\" alt=\"First Crystal\" title=\"" + gem3 + "\">");
+        $("#gem4").html("<img src=\"./assets/images/" + gem4 + ".png\" alt=\"First Crystal\" title=\"" + gem4 + "\">");
+    
 
         function calculate() {
             if (randomNum === totalScore) {
