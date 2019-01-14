9120f8edf9ec55d98eff8d7cfa76c77ff7ad3bde
AlexMukha <alexmukha@email.com>
2019-01-13 21:45:21 -0800

    Counting total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,87 @@
 $(document).ready(function() { // Document.ready.start
-var jewels = ["blue","green","yellow","pink"];
+
+var jewels = ["blue","green","yellow","pink"]; 
 var wins = "";
 var losses = "";
-var randomNum = "";
+var randomNum = 0;
+var totalScore = 0;
 var randoms = [];
 var random1 = "";
 var randomsGem = [];
+var playing = false;
 
 
+
+function play() {
+    randomNum = 19+Math.floor(Math.random()*102);
+console.log("Random number - "+randomNum);
+
+// Function to shuffle jewels array
 function shuffle(array){
-var temp;
-var i = array.length
-while(i){
-var m = Math.floor(Math.random()* i--);
-temp = array[i];
-array[i] = array[m];
-array[m] = temp;
-};
-return array;
-};
-var gems = shuffle(jewels);
-console.log(gems);
-for (var i = 0; i < gems.length; i++) {
-// console.log(gems[i]+" "+jewels[i]);
-var randomGem = 1 + Math.floor(Math.random() * 12);
-// var randomsGem = randomGem.join(",");
-randomsGem.push(randomGem);
-
-}
-// console.log(randomsGem);
-var gem1 = gems[0];
-console.log(gems[0]+" "+randomsGem[0]);
-var gem2 = gems[1];
-console.log(gems[1]+" "+randomsGem[1]);
-var gem3 = gems[2];
-console.log(gems[2]+" "+randomsGem[2]);
-var gem4 = gems[3];
-console.log(gems[3]+" "+randomsGem[3]);
-$("#gem1").html("<img src=\"./assets/images/"+gem1+".png\" alt=\"First Crystal\" title=\""+gem1+"\">");
-$("#gem2").html("<img src=\"./assets/images/"+gem2+".png\" alt=\"First Crystal\" title=\""+gem2+"\">");
-$("#gem3").html("<img src=\"./assets/images/"+gem3+".png\" alt=\"First Crystal\" title=\""+gem3+"\">");
-$("#gem4").html("<img src=\"./assets/images/"+gem4+".png\" alt=\"First Crystal\" title=\""+gem4+"\">");
-
-function game() {
-    var random1 = Math.floor(Math.random()*102)+19;
-
-console.log(random1);
-
-
-// jewels.forEach(function(gem) {
-// randomGem = Math.floor(Math.random() * jewels.length);
-// console.log(randomGem);
-// });
-
-/*
-var randomgem = Math.floor(Math.random()*jewels.length);
-console.log(randomgem);
-
-jewels.forEach(function(gem) {
-    console.log(gem);
+    var temp;
+    var i = array.length
+    while(i){
+    var m = Math.floor(Math.random()* i--);
+    temp = array[i];
+    array[i] = array[m];
+    array[m] = temp;
+    };
+    return array;
+    };
+    var gems = shuffle(jewels); // Shuffle gem order
     
-  }); */
+    // Generating a random number for each jewel
+    for (var i = 0; i < gems.length; i++) {
+    var randomGem = 1 + Math.floor(Math.random() * 12);
+    randoms.push(randomGem); // Creating an array with 4 random numbers for each jewel
+    // console.log(randomGem); // Each random number
+    // console.log(gems); // Each random number
+    // console.log(jewels); // Each random number
+    // console.log(randomGem); // Each random number
+    
+    }
+    // console.log(randoms[0]);
+    var gem1 = gems[0];
+    var gem2 = gems[1];
+    var gem3 = gems[2];
+    var gem4 = gems[3];
+    $("#gem1").html("<img src=\"./assets/images/"+gem1+".png\" alt=\"First Crystal\" title=\""+gem1+"\">");
+    $("#gem2").html("<img src=\"./assets/images/"+gem2+".png\" alt=\"First Crystal\" title=\""+gem2+"\">");
+    $("#gem3").html("<img src=\"./assets/images/"+gem3+".png\" alt=\"First Crystal\" title=\""+gem3+"\">");
+    $("#gem4").html("<img src=\"./assets/images/"+gem4+".png\" alt=\"First Crystal\" title=\""+gem4+"\">");
+}
 
-// console.log(jewels[i]);
-// console.log(random1);
+function calculate() {
 
 }
 
-game();
+$("#gem1").on("click", function() {
+    console.log("Gem 1 pressed");
+    totalScore = (randoms[0]+totalScore);
+    console.log(totalScore);
+
+calculate();
+    // console.log(totalScore);
+})
+$("#gem2").on("click", function() {
+    console.log("Gem 2 pressed");
+    totalScore = (randoms[1]+totalScore);
+    console.log(totalScore);
+})
+$("#gem3").on("click", function() {
+    console.log("Gem 3 pressed");
+    totalScore = (randoms[2]+totalScore);
+    console.log(totalScore);
+})
+$("#gem4").on("click", function() {
+    console.log("Gem 4 pressed");
+    totalScore = (randoms[3]+totalScore);
+    console.log(totalScore);
+})
+
+
+
+
+play();
 
 }); // Document.ready.end
