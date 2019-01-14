12dd83ee6a08f89f2c3b4d3cbdc8616c1cd23496
AlexMukha <alexmukha@email.com>
2019-01-13 23:41:49 -0800

    Update Readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# CrystallsCollector
-Crystalls Collector JS Game
+# Crystal collector
+
+## Rules:
+
+This game will test your math and memory at the same time.
+
+When you open the game, you will see a random number. Also you will see 4 crystals that hold a random value between 1-12.
+
+Your goal is click on cristals to figure out what value each of the crystals is holding and use the combined value to match the random number that was displayed initially.
+
+If you will be able to match the random number, you will receive a winning point. If you exceed the number, you will get a loosing point.
+
+## Good luck:
+
+
+[Play Crystal Collector](https://alexmukha.github.io/CrystallsCollector/).
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,83 @@
-$(document).ready(function() { // Document.ready.start
+$(document).ready(function () { 
 
-var jewels = ["blue","green","yellow","pink"]; 
-var wins = 0;
-var losses = 0;
-var randomNum = 0;
-var totalScore = 0;
-var randoms = [];
-var randomsGem = [];
-var playing = false;
+    var jewels = ["blue", "green", "yellow", "pink"];
+    var wins = 0;
+    var losses = 0;
+    var randomNum = 0;
+    var totalScore = 0;
+    var randoms = [];
 
+    function play() {
+        randomNum = 19 + Math.floor(Math.random() * 102);
+        $("#rNumber").text(randomNum);
 
+        // Function to shuffle jewels array
+        function shuffle(array) {
+            var temp;
+            var i = array.length
+            while (i) {
+                var m = Math.floor(Math.random() * i--);
+                temp = array[i];
+                array[i] = array[m];
+                array[m] = temp;
+            };
+            return array;
+        };
+        var gems = shuffle(jewels); // Shuffle gem order
 
-function play() {
-    randomNum = 19+Math.floor(Math.random()*102);
-    $("#rNumber").text(randomNum);
-    
-// Function to shuffle jewels array
-function shuffle(array){
-    var temp;
-    var i = array.length
-    while(i){
-    var m = Math.floor(Math.random()* i--);
-    temp = array[i];
-    array[i] = array[m];
-    array[m] = temp;
-    };
-    return array;
-    };
-    var gems = shuffle(jewels); // Shuffle gem order
-    
-    // Generating a random number for each jewel
-    for (var i = 0; i < gems.length; i++) {
-    var randomGem = 1 + Math.floor(Math.random() * 12);
-    randoms.push(randomGem); // Creating an array with 4 random numbers for each jewel
+        // Generating a random number for each jewel
+        for (var i = 0; i < gems.length; i++) {
+            var randomGem = 1 + Math.floor(Math.random() * 12);
+            randoms.push(randomGem); // Creating an array with 4 random numbers for each jewel
+        }
+        var gem1 = gems[0];
+        var gem2 = gems[1];
+        var gem3 = gems[2];
+        var gem4 = gems[3];
+        $("#gem1").html("<img src=\"./assets/images/" + gem1 + ".png\" alt=\"First Crystal\" title=\"" + gem1 + "\">");
+        $("#gem2").html("<img src=\"./assets/images/" + gem2 + ".png\" alt=\"First Crystal\" title=\"" + gem2 + "\">");
+        $("#gem3").html("<img src=\"./assets/images/" + gem3 + ".png\" alt=\"First Crystal\" title=\"" + gem3 + "\">");
+        $("#gem4").html("<img src=\"./assets/images/" + gem4 + ".png\" alt=\"First Crystal\" title=\"" + gem4 + "\">");
     }
-    var gem1 = gems[0];
-    var gem2 = gems[1];
-    var gem3 = gems[2];
-    var gem4 = gems[3];
-    $("#gem1").html("<img src=\"./assets/images/"+gem1+".png\" alt=\"First Crystal\" title=\""+gem1+"\">");
-    $("#gem2").html("<img src=\"./assets/images/"+gem2+".png\" alt=\"First Crystal\" title=\""+gem2+"\">");
-    $("#gem3").html("<img src=\"./assets/images/"+gem3+".png\" alt=\"First Crystal\" title=\""+gem3+"\">");
-    $("#gem4").html("<img src=\"./assets/images/"+gem4+".png\" alt=\"First Crystal\" title=\""+gem4+"\">");
-}
 
-function calculate() {
-if (randomNum === totalScore) {
-    wins++;
-    $("#wins").text(wins);
-    totalScore = 0;
-    randoms = [];
-    play()
-    
-} else if (randomNum > totalScore) {
-    console.log("Keep playing");
-} else {
-    losses++;
-    $("#losses").text(losses);
-    totalScore = 0;
-    randoms = [];
-    play()
-}
-$("#rNumber").text(randomNum);
-$("#guess").text(totalScore);
-}
+    function calculate() {
+        if (randomNum === totalScore) {
+            wins++;
+            $("#wins").text(wins);
+            totalScore = 0;
+            randoms = [];
+            play()
 
-$("#gem1").on("click", function() {
-    totalScore = (randoms[0]+totalScore);
-calculate();
-})
-$("#gem2").on("click", function() {
-    totalScore = (randoms[1]+totalScore);
-    calculate();
-})
-$("#gem3").on("click", function() {
-    totalScore = (randoms[2]+totalScore);
-    calculate();
-})
-$("#gem4").on("click", function() {
-    totalScore = (randoms[3]+totalScore);
-    calculate();
-})
+        } else if (randomNum > totalScore) {
+            console.log("Keep playing");
+        } else {
+            losses++;
+            $("#losses").text(losses);
+            totalScore = 0;
+            randoms = [];
+            play()
+        }
+        $("#rNumber").text(randomNum);
+        $("#guess").text(totalScore);
+    }
 
-play();
+    $("#gem1").on("click", function () {
+        totalScore = (randoms[0] + totalScore);
+        calculate();
+    })
+    $("#gem2").on("click", function () {
+        totalScore = (randoms[1] + totalScore);
+        calculate();
+    })
+    $("#gem3").on("click", function () {
+        totalScore = (randoms[2] + totalScore);
+        calculate();
+    })
+    $("#gem4").on("click", function () {
+        totalScore = (randoms[3] + totalScore);
+        calculate();
+    })
 
-}); // Document.ready.end
+    play();
+
+});
